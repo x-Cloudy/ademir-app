33168c79267bfd1d93269e8e2daef48c24cb0f4b
Juan Rodrigues <cloudibr@gmail.com>
2025-03-10 09:52:04 -0300

    FEAT: graph id get event + style

--- quasar-project/src/components/Pyramid/TreeNode.vue
+++ quasar-project/src/components/Pyramid/TreeNode.vue
@@ -1,8 +1,10 @@
 <template>
   <li>
     <!-- Nó atual -->
-    <div :style="{ backgroundImage: `url(${Logo})`}"
-    style="background-position: center center; background-size: cover; background-repeat: no-repeat; background-color: rgb(42, 42, 42);">
+    <div @click="($this: any) => console.log($this.target.className)"
+      :class="`${node.userId.id}`"
+      :style="{ backgroundImage: `url(${Logo})` }"
+      style="background-position: center center; background-size: cover; background-repeat: no-repeat; background-color: rgb(42, 42, 42);">
       <!-- Exibe a fração (n/d) -->
       <!-- {{ node.fraction.n }}/{{ node.fraction.d }} -->
       <!-- Exibe o ID do usuário, se houver -->
