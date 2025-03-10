1b5636eb625d2bdb1bcfccebce7f7cdfadaa51d7
tthiagoads <thiagoubatuba123@gmail.com>
2025-03-10 09:22:57 -0300

    FIX: /all-users

--- back/src/Controllers/User/AllUserController.ts
+++ back/src/Controllers/User/AllUserController.ts
@@ -1,17 +1,16 @@
-import {Request, Response} from "express"
+import { Request, Response } from "express";
 import { AllUserService } from "../../Services/User/AllUserService";
 
-class AllUserController{
-
-    async handle( response:Response){
+class AllUserController {
+    async handle(request: Request, response: Response) { // Recebe request e response
         const detailUserService = new AllUserService();
         const user = await detailUserService.execute();
-        response.json(user);
+        return response.status(200).json(user); // Agora response está correto
+    }
 
- }
-
-    async handle2(){
-
+    async handle2() {
+        // Lógica adicional
     }
 }
-export {AllUserController};
+
+export { AllUserController };
