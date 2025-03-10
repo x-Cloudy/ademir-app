99eab3f97061e2c1e8211e7a73890adfd4fcf2e1
tthiagoads <thiagoubatuba123@gmail.com>
2025-03-10 09:40:09 -0300

    table text

--- back/src/Controllers/User/AllUserController.ts
+++ back/src/Controllers/User/AllUserController.ts
@@ -2,14 +2,13 @@ import { Request, Response } from "express";
 import { AllUserService } from "../../Services/User/AllUserService";
 
 class AllUserController {
-    async handle(request: Request, response: Response) { // Recebe request e response
+    async handle(request: Request, response: Response) {
         const detailUserService = new AllUserService();
         const user = await detailUserService.execute();
-        return response.status(200).json(user); // Agora response está correto
+        return response.status(200).json(user);
     }
 
     async handle2() {
-        // Lógica adicional
     }
 }
 
